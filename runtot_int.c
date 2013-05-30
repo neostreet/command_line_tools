207d0bdb04bd23ef4470554d46213e71969b1513
unknown <lloyd.aidan@gmail.com>
2013-05-30 17:32:07 -0400

    add -start_bal flag

--- runtot_int.c
+++ runtot_int.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-static char usage[] = "runtot (-initial_balbal) (-verbose) filename\n";
+static char usage[] = "runtot (-initial_balbal) (-verbose) (-start_bal) filename\n";
 
 int main(int argc,char **argv)
 {
   int curr_arg;
   bool bVerbose;
+  bool bStartBal;
   FILE *fptr;
   int runtot;
   int work;
 
-  if ((argc < 2) || (argc > 4)) {
+  if ((argc < 2) || (argc > 5)) {
     printf(usage);
     return 1;
   }
 
   runtot = 0;
   bVerbose = false;
+  bStartBal = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strncmp(argv[curr_arg],"-initial_bal",12))
       sscanf(&argv[curr_arg][12],"%d",&runtot);
     else if (!strcmp(argv[curr_arg],"-verbose"))
       bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-start_bal"))
+      bStartBal = true;
     else
       break;
   }
@@ -44,12 +48,16 @@ int main(int argc,char **argv)
     if (feof(fptr))
       break;
 
-    runtot += work;
+    if (!bStartBal)
+      runtot += work;
 
     if (!bVerbose)
       printf("%d\n",runtot);
     else
       printf("%d %d\n",work,runtot);
+
+    if (bStartBal)
+      runtot += work;
   }
 
   fclose(fptr);
