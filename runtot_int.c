3f4b11be488a69cac32b9523d974fcb08f4aefa0
unknown <lloyd.aidan@gmail.com>
2013-06-08 17:52:19 -0400

    add -offset flag

--- runtot_int.c
+++ runtot_int.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-static char usage[] = "runtot (-initial_balbal) (-verbose) (-start_bal) filename\n";
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"runtot (-initial_balbal) (-verbose) (-start_bal) (-offsetoffset) filename\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
   int curr_arg;
   bool bVerbose;
   bool bStartBal;
+  int offset;
   FILE *fptr;
+  int line_len;
+  int line_no;
   int runtot;
   int work;
 
-  if ((argc < 2) || (argc > 5)) {
+  if ((argc < 2) || (argc > 6)) {
     printf(usage);
     return 1;
   }
@@ -20,6 +29,7 @@ int main(int argc,char **argv)
   runtot = 0;
   bVerbose = false;
   bStartBal = false;
+  offset = 0;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strncmp(argv[curr_arg],"-initial_bal",12))
@@ -28,6 +38,8 @@ int main(int argc,char **argv)
       bVerbose = true;
     else if (!strcmp(argv[curr_arg],"-start_bal"))
       bStartBal = true;
+    else if (!strncmp(argv[curr_arg],"-offset",7))
+      sscanf(&argv[curr_arg][7],"%d",&offset);
     else
       break;
   }
@@ -43,10 +55,12 @@ int main(int argc,char **argv)
   }
 
   for ( ; ; ) {
-    fscanf(fptr,"%d",&work);
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
 
     if (feof(fptr))
       break;
+
+    sscanf(&line[offset],"%d",&work);
 
     if (!bStartBal)
       runtot += work;
@@ -54,7 +68,7 @@ int main(int argc,char **argv)
     if (!bVerbose)
       printf("%d\n",runtot);
     else
-      printf("%d %d\n",work,runtot);
+      printf("%10d %s\n",runtot,line);
 
     if (bStartBal)
       runtot += work;
@@ -63,4 +77,28 @@ int main(int argc,char **argv)
   fclose(fptr);
 
   return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
 }
