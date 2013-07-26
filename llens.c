f8770360c45cc73f1de917ba65a2df20ff376a1d
unknown <lloyd.aidan@gmail.com>
2013-07-26 11:18:35 -0400

    add -verbose flag

--- llens.c
+++ llens.c
@@ -1,42 +1,104 @@
 #include <stdio.h>
+#include <string.h>
 
-FILE *fptr;
+#define MAX_LINE_LEN 8192
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: llens (-verbose) filename (filename ...)\n";
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
-  int n;
-  int chara;
-  int cur_lln;
+  int curr_arg;
+  bool bVerbose;
+  FILE *fptr;
+  int linelen;
+  int line_no;
+  int num_files;
+  int first_file_ix;
+  int len;
 
   if (argc < 2) {
-    printf("usage: llens filename (filename ...)\n");
+    printf(usage);
     return 1;
   }
 
-  for (n = 1; n < argc; n++) {
-    if ((fptr = fopen(argv[1],"r")) == NULL) {
-      printf("couldn't open %s\n",argv[1]);
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  num_files = argc - curr_arg;
+
+  if (num_files < 1) {
+    printf(usage);
+    return 2;
+  }
+
+  first_file_ix = curr_arg;
+
+  for ( ; curr_arg < argc; curr_arg++) {
+    if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+      printf("couldn't open %s\n",argv[curr_arg]);
       continue;
     }
 
-    cur_lln = 0;
+    if (num_files > 1) {
+      if (curr_arg > first_file_ix)
+        putchar(0x0a);
+
+      printf("%s\n",argv[curr_arg]);
+
+      for (len = strlen(argv[curr_arg]); (len > 0); len--)
+        putchar('=');
+
+      putchar(0x0a);
+    }
 
     for ( ; ; ) {
-      chara = fgetc(fptr);
+      GetLine(fptr,line,&linelen,MAX_LINE_LEN);
 
       if (feof(fptr))
         break;
 
-      if (chara == 0x0a) {
-        printf("%d\n",cur_lln);
-        cur_lln = 0;
-      }
+      line_no++;
+
+      if (!bVerbose)
+        printf("%d\n",linelen);
       else
-        cur_lln++;
+        printf("%3d %s\n",linelen,line);
     }
 
     fclose(fptr);
   }
 
   return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
 }
