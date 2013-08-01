22ddef4625b1158da50672573d54e00ef0b0b45b
unknown <lloyd.aidan@gmail.com>
2013-08-01 12:38:08 -0400

    add winning_session.c and fwinning_session.c

---
+++ fwinning_session.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_FILENAME_LEN 256
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: winning_session (-verbose) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int runtot;
+  int work;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    runtot = 0;
+    line_no = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      sscanf(line,"%d",&work);
+
+      runtot += work;
+    }
+
+    fclose(fptr);
+
+    if (runtot > 0) {
+      if (!bVerbose)
+        printf("%s\n",filename);
+      else
+        printf("%10d %s\n",runtot,filename);
+    }
+  }
+
+  fclose(fptr0);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
---
+++ winning_session.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: winning_session (-verbose) filename\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int runtot;
+  int work;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  runtot = 0;
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf("couldn't open %s\n",argv[curr_arg]);
+    return 3;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    sscanf(line,"%d",&work);
+
+    runtot += work;
+  }
+
+  fclose(fptr);
+
+  if (runtot > 0) {
+    if (!bVerbose)
+      printf("%s\n",argv[curr_arg]);
+    else
+      printf("%10d %s\n",runtot,argv[curr_arg]);
+  }
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
