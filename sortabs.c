eda01f4c7b32db1f1b3859d026e6911f32b83a8e
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-08-08 11:19:58 -0400

    add -verbose flag

--- sortabs.c
+++ sortabs.c
@@ -2,13 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define FALSE 0
-#define TRUE 1
+static char usage[] = "usage: sortabs (-descending) (-verbose) filename\n";
 
-static char usage[] = "usage: sortabs (-descending) filename\n";
+static bool bDescending;
 
-static int bDescending;
-static int *ints;
+struct val_ix {
+  int val;
+  int ix;
+};
+
+static struct val_ix *val_ixs;
 
 int compare(const void *elem1,const void *elem2);
 
@@ -16,21 +19,26 @@ int main(int argc,char **argv)
 {
   int n;
   int curr_arg;
+  bool bVerbose;
   int ix;
   FILE *fptr;
-  int num_ints;
+  int num_val_ixs;
   int work;
+  int *ixs;
 
-  if ((argc < 2) || (argc > 3)) {
+  if ((argc < 2) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
-  bDescending = FALSE;
+  bDescending = false;
+  bVerbose = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-descending"))
-      bDescending = TRUE;
+      bDescending = true;
+    else if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
     else
       break;
   }
@@ -45,7 +53,7 @@ int main(int argc,char **argv)
     return 3;
   }
 
-  num_ints = 0;
+  num_val_ixs = 0;
 
   for ( ; ; ) {
     fscanf(fptr,"%d",&work);
@@ -53,12 +61,18 @@ int main(int argc,char **argv)
     if (feof(fptr))
       break;
 
-    num_ints++;
+    num_val_ixs++;
   }
 
-  if ((ints = (int *)malloc(num_ints * sizeof (int))) == NULL) {
-    printf("couldn't malloc %d ints\n",num_ints);
+  if ((val_ixs = (struct val_ix *)malloc(num_val_ixs * sizeof (struct val_ix))) == NULL) {
+    printf("couldn't malloc %d structs\n",num_val_ixs);
     return 4;
+  }
+
+  if ((ixs = (int *)malloc(num_val_ixs * sizeof (int))) == NULL) {
+    printf("couldn't malloc %d ints\n",num_val_ixs);
+    free(val_ixs);
+    return 5;
   }
 
   fseek(fptr,0L,SEEK_SET);
@@ -71,28 +85,43 @@ int main(int argc,char **argv)
     if (feof(fptr))
       break;
 
-    ints[ix++] = work;
+    val_ixs[ix].val = work;
+    val_ixs[ix].ix = ix;
+    ix++;
   }
 
   fclose(fptr);
 
-  qsort(ints,num_ints,sizeof (int),compare);
+  for (n = 0; n < num_val_ixs; n++)
+    ixs[n] = n;
 
-  for (n = 0; n < num_ints; n++)
-    printf("%d\n",ints[n]);
+  qsort(ixs,num_val_ixs,sizeof (int),compare);
 
-  free(ints);
+  for (n = 0; n < num_val_ixs; n++) {
+    if (!bVerbose)
+      printf("%d\n",val_ixs[ixs[n]].val);
+    else
+      printf("%d (%d)\n",val_ixs[ixs[n]].val,val_ixs[ixs[n]].ix+1);
+  }
+
+  free(ixs);
+  free(val_ixs);
 
   return 0;
 }
 
 int compare(const void *elem1,const void *elem2)
 {
+  int ix1;
+  int ix2;
   int int1;
   int int2;
 
-  int1 = *(int *)elem1;
-  int2 = *(int *)elem2;
+  ix1 = *(int *)elem1;
+  ix2 = *(int *)elem2;
+
+  int1 = val_ixs[ix1].val;
+  int2 = val_ixs[ix2].val;
 
   if (int1 < 0)
     int1 *= -1;
