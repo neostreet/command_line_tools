404ff24adb34219586f39575e83a88ca24e71d1f
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-05 16:25:58 -0400

    remove reverse.c; add reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,77 @@
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: reverse infile\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int n;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  vector<string> lines;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    lines.push_back(line);
+  }
+
+  fclose(fptr);
+
+  for (n = line_no - 1; (n >= 0); n--)
+    printf("%s\n",lines[n].c_str());
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
