4d1b41cb857ba21644ca326720c48d006ce884a5
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-07 16:45:42 -0400

    scan offset in hex

--- ptch.c
+++ ptch.c
@@ -30,7 +30,7 @@ int main(int argc,char **argv)
     return 1;
   }
 
-  sscanf(argv[3],"%d",&offset);
+  sscanf(argv[3],"%x",&offset);
 
   if (!strcmp(argv[1],argv[4])) {
     printf("outfile must differ from infile\n");
