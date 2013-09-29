34d7275c3f67492782f749d93ddff1f9d17f7edb
Aidan Lloyd <lloyd.aidan@gmail.com>
2013-09-29 10:30:24 -0400

    add sortints.c

---
+++ sortints.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+static char usage[] = "usage: sortints (-descending) filename\n";
+
+static bool bDescending;
+static int *ints;
+
+int compare(const void *elem1,const void *elem2);
+
+int main(int argc,char **argv)
+{
+  int n;
+  int curr_arg;
+  int ix;
+  FILE *fptr;
+  int num_ints;
+  int work;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDescending = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-descending"))
+      bDescending = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf("couldn't open %s\n",argv[curr_arg]);
+    return 3;
+  }
+
+  num_ints = 0;
+
+  for ( ; ; ) {
+    fscanf(fptr,"%d",&work);
+
+    if (feof(fptr))
+      break;
+
+    num_ints++;
+  }
+
+  if ((ints = (int *)malloc(num_ints * sizeof (int))) == NULL) {
+    printf("couldn't malloc %d ints\n",num_ints);
+    return 4;
+  }
+
+  fseek(fptr,0L,SEEK_SET);
+
+  ix = 0;
+
+  for ( ; ; ) {
+    fscanf(fptr,"%d",&work);
+
+    if (feof(fptr))
+      break;
+
+    ints[ix++] = work;
+  }
+
+  fclose(fptr);
+
+  qsort(ints,num_ints,sizeof (int),compare);
+
+  for (n = 0; n < num_ints; n++)
+    printf("%d\n",ints[n]);
+
+  free(ints);
+
+  return 0;
+}
+
+int compare(const void *elem1,const void *elem2)
+{
+  int int1;
+  int int2;
+
+  int1 = *(int *)elem1;
+  int2 = *(int *)elem2;
+
+  if (!bDescending)
+    return int1 - int2;
+
+  return int2 - int1;
+}
