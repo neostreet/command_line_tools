a43a2d5c0cf9535a349d8bf27aaa7866cf2d5907
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-09-14 21:01:25 -0400

    add delta.c

---
+++ delta.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+#define MAX_LINE_LEN 1024
+static char save_line[MAX_LINE_LEN];
+
+static char usage[] = "usage: delta (-verbose) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int old_balance;
+  int balance;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d",&balance);
+
+    if (line_no == 1) {
+      old_balance = balance;
+      strcpy(save_line,line);
+      continue;
+    }
+
+    if (!bVerbose)
+      printf("%d\n",balance - old_balance);
+    else
+      printf("%d %s\n",balance - old_balance,save_line);
+
+    old_balance = balance;
+    strcpy(save_line,line);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
