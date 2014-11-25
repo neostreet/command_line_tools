0cba2059088e01142ad8ce96b94a772740db9a62
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-11-24 22:21:16 -0500

    add gap argument

--- twin_lines.c
+++ twin_lines.c
@@ -2,35 +2,48 @@
 #include <string.h>
 
 #define MAX_LINE_LEN 1024
-static char line[3][MAX_LINE_LEN];
+#define MAX_CONTIG 10
+static char line[MAX_CONTIG][MAX_LINE_LEN];
 
-static char usage[] = "usage: twin_lines filename\n";
+static char usage[] =
+"usage: twin_lines gap filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int gap;
+  int contig;
   FILE *fptr;
   int line_len;
   int line_no;
   int curr_ix;
   int compare_ix;
 
-  if (argc != 2) {
+  if (argc != 3) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  sscanf(argv[1],"%d",&gap);
+
+  contig = gap + 1;
+
+  if (contig > MAX_CONTIG) {
+    printf("gap must be <= %d\n",MAX_CONTIG - 1);
     return 2;
+  }
+
+  if ((fptr = fopen(argv[2],"r")) == NULL) {
+    printf(couldnt_open,argv[2]);
+    return 3;
   }
 
   line_no = 0;
 
   for ( ; ; ) {
-    curr_ix = line_no % 3;
+    curr_ix = line_no % contig;
 
     GetLine(fptr,line[curr_ix],&line_len,MAX_LINE_LEN);
 
@@ -39,21 +52,11 @@ int main(int argc,char **argv)
 
     line_no++;
 
-    if (line_no >= 3) {
-      switch (curr_ix) {
-        case 0:
-          compare_ix = 1;
+    if (line_no >= contig) {
+      compare_ix = curr_ix - gap;
 
-          break;
-        case 1:
-          compare_ix = 2;
-
-          break;
-        case 2:
-          compare_ix = 0;
-
-          break;
-      }
+      if (compare_ix < 0)
+        compare_ix += contig;
 
       if (!strcmp(line[curr_ix],line[compare_ix]))
         printf("%7d %s\n",line_no,line[curr_ix]);
