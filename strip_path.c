77fa628d8c840d4d519e026aee15fc7d47f806d4
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-01-06 20:15:41 -0500

    add strip_path.c

---
+++ strip_path.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: strip_path filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static char *strip_path(char *line,int line_len);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    printf("%s\n",strip_path(line,line_len));
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static char *strip_path(char *line,int line_len)
+{
+   int n;
+
+   for (n = line_len - 1; (n >= 0); n--) {
+     if (line[n] == '\\')
+       break;
+   }
+
+   return &line[n+1];
+}
