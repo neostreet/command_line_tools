fb3db95a783f9cc069e21d1c1e65b7a8672108ef
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-07-03 11:34:58 -0400

    add run_win_pct.c

---
+++ run_win_pct.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+
+static char usage[] =
+"usage: run_win_pct (-verbose) filename\n";
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  FILE *fptr;
+  int line_len;
+  int nobs;
+  int wins;
+  int work;
+  double win_pct;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf("couldn't open %s\n",argv[curr_arg]);
+    return 3;
+  }
+
+  nobs = 0;
+  wins = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    sscanf(line,"%d",&work);
+
+    if (feof(fptr))
+      break;
+
+    nobs++;
+
+    if (work > 0)
+      wins++;
+
+    win_pct = (double)wins / (double)nobs;
+
+    if (!bVerbose)
+      printf("%8.4lf\n",win_pct);
+    else
+      printf("%8.4lf %s\n",win_pct,line);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
