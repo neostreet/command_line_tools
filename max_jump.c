01c965f48a464274c88eddbb6ae0460759a1218f
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-08-18 21:47:36 -0400

    add max_jump.c and sum_max_drop_and_max_jump.c

---
+++ max_jump.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
+
+#define MAX_LINE_LEN 1024
+char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: max_jump (-debug) (-verbose) (-offsetoffset) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  bool bVerbose;
+  int offset;
+  FILE *fptr;
+  int linelen;
+  int line_no;
+  int work;
+  int jump;
+  int min;
+  int max_jump;
+
+  if ((argc < 2) || (argc > 5)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+  bVerbose = false;
+  offset = 0;
+  min = 0;
+  max_jump = 0;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else if (!strncmp(argv[curr_arg],"-offset",7))
+      sscanf(&argv[curr_arg][7],"%d",&offset);
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if (bDebug)
+    getcwd(save_dir,_MAX_PATH);
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&linelen,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(&line[offset],"%d",&work);
+
+    if ((line_no == 1) || (work <= min))
+      min = work;
+    else {
+      jump = work - min;
+
+      if (jump > max_jump)
+        max_jump = jump;
+    }
+  }
+
+  fclose(fptr);
+
+  if (!bDebug)
+    printf("%d\n",max_jump);
+  else
+    printf("%d %s\n",max_jump,save_dir);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
