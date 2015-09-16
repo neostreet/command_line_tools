8a7504c0e6da8fdee712b23e0e1f9993129e8365
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-09-15 21:44:40 -0400

    add agg_percentile.c

---
+++ agg_percentile.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: agg_percentile filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int tot_entries;
+  int tot_place;
+  int entries;
+  int place;
+  double percentile;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+  tot_entries = 0;
+  tot_place = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d %d",&entries,&place);
+
+    tot_entries += entries;
+    tot_place += place;
+  }
+
+  fclose(fptr);
+
+  percentile = (double)(tot_entries - tot_place) / (double)tot_entries;
+
+  printf("%d %d %lf\n",tot_entries,tot_place,percentile);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
