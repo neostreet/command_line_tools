a3ac91928fc3a374b2c680f77c6d2d9f96049ac0
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-09-29 21:35:06 -0400

    add -lastn flag

--- min_int.c
+++ min_int.c
@@ -14,7 +14,7 @@ static char save_dir[_MAX_PATH];
 char line[MAX_LINE_LEN];
 
 static char usage[] =
-"usage: min_int (-debug) (-verbose) (-offsetoffset) filename\n";
+"usage: min_int (-verbose) (-offsetoffset) (-lastn) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int minllen);
@@ -22,32 +22,34 @@ static void GetLine(FILE *fptr,char *line,int *line_len,int minllen);
 int main(int argc,char **argv)
 {
   int curr_arg;
-  bool bDebug;
   bool bVerbose;
   int offset;
+  int lastn;
   FILE *fptr;
   int linelen;
   int line_no;
   int work;
   int min;
+  int min_ix;
 
   if ((argc < 2) || (argc > 5)) {
     printf(usage);
     return 1;
   }
 
-  bDebug = false;
   bVerbose = false;
   offset = 0;
-  min = 0;
+  lastn = 0;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-debug"))
-      bDebug = true;
-    else if (!strcmp(argv[curr_arg],"-verbose"))
+    if (!strcmp(argv[curr_arg],"-verbose")) {
       bVerbose = true;
+      getcwd(save_dir,_MAX_PATH);
+    }
     else if (!strncmp(argv[curr_arg],"-offset",7))
       sscanf(&argv[curr_arg][7],"%d",&offset);
+    else if (!strncmp(argv[curr_arg],"-last",5))
+      sscanf(&argv[curr_arg][5],"%d",&lastn);
     else
       break;
   }
@@ -57,15 +59,13 @@ int main(int argc,char **argv)
     return 2;
   }
 
-  if (bDebug)
-    getcwd(save_dir,_MAX_PATH);
-
   if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
     printf(couldnt_open,argv[curr_arg]);
     return 3;
   }
 
   line_no = 0;
+  min = 0;
 
   for ( ; ; ) {
     GetLine(fptr,line,&linelen,MAX_LINE_LEN);
@@ -77,16 +77,20 @@ int main(int argc,char **argv)
 
     sscanf(&line[offset],"%d",&work);
 
-    if ((line_no == 1) || (work < min))
+    if ((line_no == 1) || (work < min)) {
       min = work;
+      min_ix = line_no;
+    }
   }
 
   fclose(fptr);
 
-  if (!bDebug)
-    printf("%d\n",min);
-  else
-    printf("%d %s\n",min,save_dir);
+  if (!lastn || (line_no - min_ix + 1 <= lastn)) {
+    if (!bVerbose)
+      printf("%d\n",min);
+    else
+      printf("%d %d %d %s\n",min,min_ix,line_no,save_dir);
+  }
 
   return 0;
 }
