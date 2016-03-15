30ff268cd71c4a3feaf532cc467ba16b0cf3e234
Aidan Lloyd <lloyd.aidan@gmail.com>
2016-03-14 22:08:21 -0400

    add denomination info

--- scr_grab_board.c
+++ scr_grab_board.c
@@ -2,13 +2,64 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LEFT      0
-#define TOP       1
-#define WIDTH     2
-#define NUM_PARMS 3
+struct left_top {
+  int left;
+  int top;
+};
+
+#define NUM_LEFT_TOPS 3
+static struct left_top left_tops[NUM_LEFT_TOPS];
 
 static char usage[] =
-"usage: scr_grab_board (-debug) (-abbrev) left top width\n";
+"usage: scr_grab_board (-debug) (-abbrev) left1 top1 left2 top2 left3 top3 width\n";
+
+static char *denoms[] = {
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "jack",
+  "queen",
+  "king",
+  "ace"
+};
+#define NUM_DENOMS (sizeof denoms / sizeof (char *))
+
+static char *denom_abbrevs = "23456789TJQKA";
+
+static int denom_colors[][2] = {
+  { 0x10101, 0x0 },
+  { 0x5f5f5f, 0xa0a0a },
+  { 0xffffff, 0x9d9d9d },
+  { 0x70707, 0x70707 },
+  { 0x2e2e2e, 0x40404 },
+  { 0xffffff, 0x0 },
+  { 0x636363, 0xa0a0a },
+  { 0xb0b0b, 0x40404 },
+  { 0x0, 0x40404 },
+  { 0x282828, 0xffffff },
+  { 0x626262, 0x50505 },
+  { 0x0, 0xffffff },
+  { 0x0, 0x0 },
+  { 0x1111ca, 0x1010ca },
+  { 0x6969dd, 0x1a1acc },
+  { 0xffffff, 0xa3a3ea },
+  { 0x1717cb, 0x1717cb },
+  { 0x3b3bd3, 0x1414cb },
+  { 0xffffff, 0x1010ca },
+  { 0x6d6dde, 0x1a1acc },
+  { 0x1b1bcc, 0x1414cb },
+  { 0x1010ca, 0x1414cb },
+  { 0x3636d2, 0xffffff },
+  { 0x6c6cde, 0x1515cb },
+  { 0x1010ca, 0xffffff },
+  { 0x1010ca, 0x1010ca }
+};
 
 static char *suits[] = {
   "clubs",
@@ -26,7 +77,6 @@ static int suit_colors[] = {
   0xffffff,
   0xe8e8e8
 };
-
 static char couldnt_open[] = "couldn't open %s\n";
 static char fmt[] = "%08x\n";
 
@@ -37,11 +87,11 @@ int main(int argc,char **argv)
   int curr_arg;
   bool bDebug;
   bool bAbbrev;
-  int parms[NUM_PARMS];
+  int width;
   HDC hDC;
-  COLORREF color;
+  COLORREF colors[NUM_LEFT_TOPS];
 
-  if ((argc < 4) || (argc > 6)) {
+  if ((argc < 8) || (argc > 10)) {
     printf(usage);
     return 1;
   }
@@ -58,7 +108,7 @@ int main(int argc,char **argv)
       break;
   }
 
-  if (argc - curr_arg != 3) {
+  if (argc - curr_arg != 7) {
     printf(usage);
     return 2;
   }
@@ -70,31 +120,46 @@ int main(int argc,char **argv)
     return 3;
   }
 
-  for (n = 0; n < NUM_PARMS; n++)
-    sscanf(argv[curr_arg + n],"%d",&parms[n]);
-
-  if (bDebug) {
-    printf("parms[LEFT]   = %d\n",parms[LEFT]);
-    printf("parms[TOP]    = %d\n",parms[TOP]);
-    printf("parms[WIDTH]  = %d\n",parms[WIDTH]);
+  for (n = 0; n < NUM_LEFT_TOPS; n++) {
+    sscanf(argv[curr_arg + n*2],"%d",&left_tops[n].left);
+    sscanf(argv[curr_arg + n*2 + 1],"%d",&left_tops[n].top);
   }
 
-  for (m = 0; m < 5; m++) {
-    color = GetPixel(hDC,parms[LEFT] + m * parms[WIDTH],parms[TOP]);
+  sscanf(argv[argc-1],"%d",&width);
 
-    if (bDebug)
-      printf("color = %x\n",color);
+  for (m = 0; m < 5; m++) {
+    for (n = 0; n < NUM_LEFT_TOPS; n++) {
+      colors[n] = GetPixel(hDC,left_tops[n].left + m * width,left_tops[n].top);
+
+      if (bDebug)
+        printf("colors[%d] = %x\n",n,colors[n]);
+    }
+
+    for (n = 0; n < NUM_DENOMS * 2; n++) {
+      if ((denom_colors[n][0] == colors[0]) && (denom_colors[n][1] == colors[1]))
+        break;
+    }
+
+    if (n == NUM_DENOMS * 2)
+      break;
+
+    if (m)
+      putchar(' ');
+
+    n = n % NUM_DENOMS;
+
+    if (bAbbrev)
+      printf("%c",denom_abbrevs[n]);
+    else
+      printf("%s",denoms[n]);
 
     for (n = 0; n < NUM_SUITS; n++) {
-      if (color == suit_colors[n])
+      if (colors[2] == suit_colors[n])
         break;
     }
 
     if (n == NUM_SUITS)
       break;
-
-    if (m)
-      putchar(' ');
 
     if (bAbbrev)
       printf("%c",suit_abbrevs[n]);
