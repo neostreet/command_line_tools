a7762a053d00f02fd67668289dd0a587de7e862d
Aidan Lloyd <lloyd.aidan@gmail.com>
2016-08-26 21:14:42 -0400

    add run_delta_pct.c

---
+++ run_delta_pct.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+
+static char usage[] =
+"usage: run_delta_pct (-verbose) start_bal filename\n";
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  int start_bal;
+  FILE *fptr;
+  int balance;
+  int line_len;
+  int work;
+  double delta_pct;
+
+  if ((argc < 3) || (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg+1],"r")) == NULL) {
+    printf("couldn't open %s\n",argv[curr_arg+1]);
+    return 3;
+  }
+
+  sscanf(argv[curr_arg],"%d",&start_bal);
+  balance = start_bal;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    sscanf(line,"%d",&work);
+
+    if (feof(fptr))
+      break;
+
+    delta_pct = (double)work / (double)balance;
+
+    if (!bVerbose)
+      printf("%8.4lf\n",delta_pct);
+    else
+      printf("%8.4lf (%d %d)\n",delta_pct,work,balance);
+
+    balance += work;
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
