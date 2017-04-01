8766dcc2db3ac77149060c56d00183ea23f25155
Aidan Lloyd <lloyd.aidan@gmail.com>
2017-04-01 10:13:20 -0400

    add -only_starting flag

--- runtot_int6.c
+++ runtot_int6.c
@@ -4,7 +4,8 @@
 #define MAX_STR_LEN 256
 
 static char usage[] =
-"runtot_int6 (-initial_balbal) (-verbose) (-no_tabs) (-only_ending) filename\n";
+"runtot_int6 (-initial_balbal) (-verbose) (-no_tabs) (-only_ending)\n"
+"  (-ony_starting) filename\n";
 
 int main(int argc,char **argv)
 {
@@ -12,12 +13,13 @@ int main(int argc,char **argv)
   bool bVerbose;
   bool bNoTabs;
   bool bOnlyEnding;
+  bool bOnlyStarting;
   FILE *fptr;
   int runtot;
   int work;
   char str[MAX_STR_LEN];
 
-  if ((argc < 2) || (argc > 6)) {
+  if ((argc < 2) || (argc > 7)) {
     printf(usage);
     return 1;
   }
@@ -26,6 +28,7 @@ int main(int argc,char **argv)
   bVerbose = false;
   bNoTabs = false;
   bOnlyEnding = false;
+  bOnlyStarting = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strncmp(argv[curr_arg],"-initial_bal",12))
@@ -36,6 +39,8 @@ int main(int argc,char **argv)
       bNoTabs = true;
     else if (!strcmp(argv[curr_arg],"-only_ending"))
       bOnlyEnding = true;
+    else if (!strcmp(argv[curr_arg],"-only_starting"))
+      bOnlyStarting = true;
     else
       break;
   }
@@ -45,9 +50,14 @@ int main(int argc,char **argv)
     return 2;
   }
 
+  if (bOnlyEnding && bOnlyStarting) {
+    printf("cant' specify both -only_ending and -only_starting\n");
+    return 3;
+  }
+
   if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
     printf("couldn't open %s\n",argv[curr_arg]);
-    return 3;
+    return 4;
   }
 
   for ( ; ; ) {
@@ -64,20 +74,24 @@ int main(int argc,char **argv)
     if (!bOnlyEnding) {
       if (!bVerbose) {
         if (!bNoTabs)
-          printf("%d\t",runtot);
+          printf("%d",runtot);
         else
-          printf("%10d ",runtot);
+          printf("%10d",runtot);
       }
       else
-        printf("%d\t%d\t",work,runtot);
+        printf("%d\t%d",work,runtot);
     }
 
     runtot += work;
 
-    if (bVerbose || !bNoTabs)
-      printf("%d\n",runtot);
-    else
-      printf("%10d\n",runtot);
+    if (bOnlyStarting)
+      putchar(0x0a);
+    else {
+      if (bVerbose || !bNoTabs)
+        printf("\t%d\n",runtot);
+      else
+        printf(" %10d\n",runtot);
+    }
   }
 
   fclose(fptr);
