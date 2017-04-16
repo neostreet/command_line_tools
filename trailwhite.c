f9930e4d445769d6045f882ffff75eb22e13266c
Aidan Lloyd <lloyd.aidan@gmail.com>
2017-04-16 14:32:31 -0400

    add trailwhite.c

---
+++ trailwhite.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+#define TAB 0x09
+
+#define MAX_LINE_LEN 4096
+char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: trailwhite filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int linelen;
+  int line_no;
+  int chara;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&linelen,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    chara = line[linelen-1];
+
+    if ((chara == TAB) || (chara == ' '))
+      printf("%03d:%s\n",line_no,line);
+  }
+}
+
+void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
