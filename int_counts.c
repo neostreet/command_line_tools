84c0ff08450a31da8bb39e3d4217975111f0b887
Aidan Lloyd <lloyd.aidan@gmail.com>
2017-10-02 21:21:08 -0400

    add int_counts.c

---
+++ int_counts.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+#define MAX_COUNTS 1000
+static int int_counts[MAX_COUNTS];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: int_counts filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int n;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int work;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d",&work);
+
+    if ((work < 0) || (work > MAX_COUNTS)) {
+      printf("invalid int at line %d: %d\n",line_no,work);
+      return 3;
+    }
+
+    work--;
+    int_counts[work]++;
+  }
+
+  fclose(fptr);
+
+  for (n = 0; n < MAX_COUNTS; n++) {
+    if (int_counts[n])
+      printf("%4d: %10d\n",n+1,int_counts[n]);
+  }
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
