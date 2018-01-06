ab1445547864f6e15026a4cd96f484f12b0bea24
Aidan Lloyd <lloyd.aidan@gmail.com>
2018-01-05 22:16:14 -0500

    add -debug flag

--- count_pos.c
+++ count_pos.c
@@ -1,30 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
 
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: count_pos filename\n";
+static char usage[] = "usage: count_pos (-debug) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
+  bool bDebug;
   FILE *fptr;
   int line_len;
   int line_no;
   int count_pos;
   int work;
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
     return 2;
   }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  if (bDebug)
+    getcwd(save_dir,_MAX_PATH);
 
   count_pos = 0;
   line_no = 0;
@@ -44,7 +72,10 @@ int main(int argc,char **argv)
 
   fclose(fptr);
 
-  printf("%d\n",count_pos);
+  if (!bDebug)
+    printf("%d\n",count_pos);
+  else
+    printf("%d %s\n",count_pos,save_dir);
 
   return 0;
 }
