fbb87891221bd66cf213364f141a0da31ca8525e
Aidan Lloyd <lloyd.aidan@gmail.com>
2018-04-26 21:19:10 -0400

    add -zero flag

--- make_file_of_size.c
+++ make_file_of_size.c
@@ -14,7 +14,7 @@
 #endif
 
 static char usage[] =
-"usage: make_file_of_size size filename\n";
+"usage: make_file_of_size (-zero) size filename\n";
 
 static char couldnt_open[] = "couldn't open %s\n";
 static char couldnt_get_status[] = "couldn't get status of %s\n";
@@ -22,43 +22,67 @@ static char couldnt_get_status[] = "couldn't get status of %s\n";
 int main(int argc,char **argv)
 {
   int n;
-  int to_do;
+  int curr_arg;
+  bool bZero;
+  unsigned int to_do;
   off_t bytes_to_do;
+  unsigned int per_chunk;
   off_t bytes_per_chunk;
+  unsigned int last_chunk;
   off_t last_chunk_bytes;
   char *buf;
   int num_chunks;
   int fhndl;
   int bytes_read;
 
-  if (argc != 3) {
+  if ((argc < 3) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
-  sscanf(argv[1],"%d",&to_do);
-  bytes_to_do = to_do;
+  bZero = false;
 
-  if ((fhndl = open(argv[2],
-    O_CREAT | O_EXCL | O_BINARY | O_WRONLY,
-    S_IREAD | S_IWRITE)) == -1) {
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-zero"))
+      bZero = true;
+    else
+      break;
+  }
 
-    printf(couldnt_open,argv[2]);
+  if (argc - curr_arg != 2) {
+    printf(usage);
     return 2;
   }
 
-  bytes_per_chunk = 1048576;
+  sscanf(argv[curr_arg],"%u",&to_do);
+  bytes_to_do = (off_t)to_do;
 
-  if ((buf = (char *)malloc(bytes_per_chunk)) == NULL) {
-    printf("malloc of %d bytes failed\n",bytes_per_chunk);
+  if ((fhndl = open(argv[curr_arg+1],
+    O_CREAT | O_EXCL | O_BINARY | O_WRONLY,
+    S_IREAD | S_IWRITE)) == -1) {
+
+    printf(couldnt_open,argv[curr_arg+1]);
     return 3;
   }
 
-  for (n = 0; n < bytes_per_chunk; n++)
-    buf[n] = 'a' + (n % 26);
+  per_chunk = 1048576;
+  bytes_per_chunk = (off_t)per_chunk;
 
-  num_chunks = (bytes_to_do + bytes_per_chunk - 1) / bytes_per_chunk;
-  last_chunk_bytes = bytes_to_do - (num_chunks - 1) * bytes_per_chunk;
+  if ((buf = (char *)malloc(per_chunk)) == NULL) {
+    printf("malloc of %d bytes failed\n",per_chunk);
+    return 4;
+  }
+
+  for (n = 0; n < per_chunk; n++) {
+    if (bZero)
+      buf[n] = 0;
+    else
+      buf[n] = 'a' + (n % 26);
+  }
+
+  num_chunks = (to_do + per_chunk - 1) / per_chunk;
+  last_chunk = to_do - (num_chunks - 1) * per_chunk;
+  last_chunk_bytes = (off_t)last_chunk;
 
   for (n = 0; n < num_chunks; n++) {
     write(fhndl,buf,
