082b5130ea3b12ee3c3b5bc9c151e9d750a2ae28
Aidan Lloyd <lloyd.aidan@gmail.com>
2019-08-12 22:59:37 -0400

    add peak_ix.c

---
+++ peak_ix.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+
+static char save_dir[_MAX_PATH];
+
+#define MAX_LINE_LEN 1024
+char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: peak_ix (-debug) (-terse) (-verbose) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  bool bTerse;
+  bool bVerbose;
+  FILE *fptr;
+  int linelen;
+  int line_no;
+  int work;
+  int total;
+  int peak;
+  int peak_ix;
+
+  if ((argc < 2) || (argc > 5)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+  bTerse = false;
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else if (!strcmp(argv[curr_arg],"-terse"))
+      bTerse = true;
+    else if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if (bTerse && bVerbose) {
+    printf("can't specify both -terse and -verbose\n");
+    return 3;
+  }
+
+  getcwd(save_dir,_MAX_PATH);
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
+  }
+
+  line_no = 0;
+
+  total = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&linelen,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+    sscanf(&line[0],"%d",&work);
+
+    total += work;
+
+    if ((line_no == 1) || (total > peak)) {
+      peak = total;
+      peak_ix = line_no - 1;
+    }
+  }
+
+  fclose(fptr);
+
+  if (!bVerbose)
+    printf("%d\n",peak_ix);
+  else
+    printf("%d %s\n",peak_ix,save_dir);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
