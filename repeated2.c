a83241f57d20b8d0dd2d3e160322d2008ecad0a2
Aidan Lloyd <lloyd.aidan@gmail.com>
2020-08-08 15:16:14 -0400

    add repeated2.c

---
+++ repeated2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 8192
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: repeated2 filename\n";
+
+void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int val;
+  int prev_val;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf("couldn't open %s\n",argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%d",&val);
+
+    if ((line_no > 1) && (val == prev_val)) {
+      printf("%d: %s\n",line_no,line);
+    }
+
+    prev_val = val;
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
