4bd4138dfc8e3f12997d9e676ebd01143a1a6262
Aidan Lloyd <lloyd.aidan@gmail.com>
2020-09-01 21:52:09 -0400

    add -no_line_numbers flag

--- repeated2.c
+++ repeated2.c
@@ -4,26 +4,42 @@
 #define MAX_LINE_LEN 8192
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: repeated2 filename\n";
+static char usage[] = "usage: repeated2 (-no_line_numbers) filename\n";
 
 void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
+  bool bNoLineNumbers;
   FILE *fptr;
   int line_len;
   int line_no;
   int val;
   int prev_val;
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf("couldn't open %s\n",argv[1]);
+  bNoLineNumbers = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-no_line_numbers"))
+      bNoLineNumbers = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
     return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf("couldn't open %s\n",argv[curr_arg]);
+    return 3;
   }
 
   line_no = 0;
@@ -39,7 +55,10 @@ int main(int argc,char **argv)
     sscanf(line,"%d",&val);
 
     if ((line_no > 1) && (val == prev_val)) {
-      printf("%d: %s\n",line_no,line);
+      if (!bNoLineNumbers)
+        printf("%d: %s\n",line_no,line);
+      else
+        printf("%s\n",line);
     }
 
     prev_val = val;
