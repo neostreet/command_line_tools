e8312d1af5a00f6f6a5e9982dea2a70ffa29eaed
neostreet <lloyd.aidan@gmail.com>
2024-12-14 20:16:35 -0500

    add roi.c

---
+++ roi.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+#define DATE_LEN 10
+static char date[DATE_LEN+1];
+
+static char usage[] = "usage: roi filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int starting_balance;
+  int delta;
+  double roi;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    sscanf(line,"%s\t%d\t%d",date,&starting_balance,&delta);
+
+    roi = (double)delta / (double)starting_balance;
+
+    printf("%lf %s\t%d\t%d\n",roi,date,starting_balance,delta);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
