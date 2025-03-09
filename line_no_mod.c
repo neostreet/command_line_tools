cb531f8f25306b8dafb09eef530a497efa42c922
neostreet <lloyd.aidan@gmail.com>
2025-03-08 22:56:16 -0500

    add -verbose flag

--- line_no_mod.c
+++ line_no_mod.c
@@ -1,31 +1,48 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: line_no_mod modulus ix filename\n";
+static char usage[] = "usage: line_no_mod (-verbose) modulus ix filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
+  bool bVerbose;
   int modulus;
   int ix;
   FILE *fptr;
   int line_len;
   int line_no;
 
-  if (argc != 4) {
+  if ((argc < 4) || (argc > 5)) {
     printf(usage);
     return 1;
   }
 
-  sscanf(argv[1],"%d",&modulus);
-  sscanf(argv[2],"%d",&ix);
+  bVerbose = false;
 
-  if ((fptr = fopen(argv[3],"r")) == NULL) {
-    printf(couldnt_open,argv[3]);
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 3) {
+    printf(usage);
+    return 2;
+  }
+
+  sscanf(argv[curr_arg],"%d",&modulus);
+  sscanf(argv[curr_arg+1],"%d",&ix);
+
+  if ((fptr = fopen(argv[curr_arg+2],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg+2]);
     return 2;
   }
 
@@ -37,8 +54,12 @@ int main(int argc,char **argv)
     if (feof(fptr))
       break;
 
-    if ((line_no % modulus) == ix)
-      printf("%s\n",line);
+    if ((line_no % modulus) == ix) {
+      if (!bVerbose)
+        printf("%s\n",line);
+      else
+        printf("%d %s\n",line_no,line);
+    }
 
     line_no++;
   }
