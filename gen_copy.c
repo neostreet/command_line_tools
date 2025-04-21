a1673ce75251219112bdb27ff08520a86b598c6f
neostreet <lloyd.aidan@gmail.com>
2025-04-20 20:39:35 -0400

    add gen_copy.c

---
+++ gen_copy.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+static char save_line[MAX_LINE_LEN];
+
+static char usage[] = "usage: gen_copy filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_len;
+  int line_no;
+
+  if (argc != 2) {
+    printf(usage);
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  printf("@echo off\n");
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    if (line_no == 2) {
+      printf("copy /b %s+%s gen_copy_temp1\n",save_line,line);
+    }
+    else if (line_no > 2) {
+      printf("copy /b gen_copy_temp%d+%s gen_copy_temp%d\n",line_no-2,line,line_no-1);
+    }
+
+    strcpy(save_line,line);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
