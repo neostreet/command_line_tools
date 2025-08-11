47d92633e66c91800c5edc5b5a4345ed9a8b50d1
neostreet <lloyd.aidan@gmail.com>
2025-08-11 12:42:40 -0400

    support multiple files

--- skeleton.c
+++ skeleton.c
@@ -3,41 +3,44 @@
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: skeleton filename\n";
+static char usage[] = "usage: skeleton filename (filename ...)\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int n;
   FILE *fptr;
   int line_len;
   int line_no;
 
-  if (argc != 2) {
+  if (argc < 2) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
-    return 2;
+  for (n = 1; n < argc; n++) {
+    if ((fptr = fopen(argv[n],"r")) == NULL) {
+      printf(couldnt_open,argv[n]);
+      return 2;
+    }
+
+    line_no = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      printf("%s\n",line);
+    }
+
+    fclose(fptr);
   }
-
-  line_no = 0;
-
-  for ( ; ; ) {
-    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
-
-    if (feof(fptr))
-      break;
-
-    line_no++;
-
-    printf("%3d: %s\n",line_no,line);
-  }
-
-  fclose(fptr);
 
   return 0;
 }
