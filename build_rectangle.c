4003dd7b1ae580009b7cd61c00762cec8ce38899
neostreet <lloyd.aidan@gmail.com>
2025-12-03 21:28:09 -0500

    add build_rectangle.c

---
+++ build_rectangle.c
@@ -0,0 +1,57 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#ifdef WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+#define LEFT      0
+#define TOP       1
+#define WIDTH     2
+#define HEIGHT    3
+#define NUM_PARMS 4
+
+static char usage[] = "usage: build_rectangle left top width height color1 color2\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+int main(int argc,char **argv)
+{
+  int m;
+  int n;
+  int parms[NUM_PARMS];
+  COLORREF colors[2];
+
+  if (argc != 7) {
+    printf(usage);
+    return 1;
+  }
+
+  for (n = 0; n < NUM_PARMS; n++)
+    sscanf(argv[1 + n],"%d",&parms[n]);
+
+  for (n = 0; n < NUM_PARMS; n++)
+    printf("%d\n",parms[n]);
+
+  for (m = 0; m < parms[HEIGHT]; m++) {
+    if (!m || (m == parms[HEIGHT] - 1)) {
+      for (n = 0; n < parms[WIDTH]; n++)
+        printf("%s\n",argv[5]);
+    }
+    else {
+      printf("%s\n",argv[5]);
+
+      for (n = 0; n < parms[WIDTH] - 2; n++)
+        printf("%s\n",argv[6]);
+
+      printf("%s\n",argv[5]);
+    }
+  }
+
+  return 0;
+}
