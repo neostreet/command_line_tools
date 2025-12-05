82cfabc9ccb5420dbe6f5b14623910a55d0c6967
neostreet <lloyd.aidan@gmail.com>
2025-12-04 20:17:42 -0500

    hard-code the background color to white

--- build_rectangle.c
+++ build_rectangle.c
@@ -1,15 +1,4 @@
-#include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#ifdef WIN32
-#include <io.h>
-#else
-#include <unistd.h>
-#endif
 
 #define LEFT      0
 #define TOP       1
@@ -17,7 +6,7 @@
 #define HEIGHT    3
 #define NUM_PARMS 4
 
-static char usage[] = "usage: build_rectangle left top width height color1 color2\n";
+static char usage[] = "usage: build_rectangle left top width height color\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 int main(int argc,char **argv)
@@ -25,9 +14,8 @@ int main(int argc,char **argv)
   int m;
   int n;
   int parms[NUM_PARMS];
-  COLORREF colors[2];
 
-  if (argc != 7) {
+  if (argc != 6) {
     printf(usage);
     return 1;
   }
@@ -47,7 +35,7 @@ int main(int argc,char **argv)
       printf("%s\n",argv[5]);
 
       for (n = 0; n < parms[WIDTH] - 2; n++)
-        printf("%s\n",argv[6]);
+        printf("00ffffff\n");
 
       printf("%s\n",argv[5]);
     }
