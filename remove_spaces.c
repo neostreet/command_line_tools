6de80c3cec4d620581831e53182f9cc606d40c72
neostreet <lloyd.aidan@gmail.com>
2026-09-15 21:34:39 -0400

    add remove_spaces.c

---
+++ remove_spaces.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: remove_spaces (-debug) infile outfile\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int remove_spaces(char *line,int line_len);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  FILE *fptr;
+  FILE *out_fptr;
+  int line_len;
+  int line_no;
+  int count;
+  int total_count;
+
+  if ((argc < 3) || (argc > 4)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else
+       break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
+    return 2;
+  }
+
+  if (!strcmp(argv[curr_arg],argv[curr_arg+1])) {
+    printf("outfile must differ from infile\n");
+    return 3;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
+  }
+
+  if ((out_fptr = fopen(argv[curr_arg+1],"w")) == NULL) {
+    printf(couldnt_open,argv[curr_arg+1]);
+    return 5;
+  }
+
+  total_count = 0;
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    count = remove_spaces(line,line_len);
+    total_count += count;
+    fprintf(out_fptr,"%s\n",line);
+  }
+
+  fclose(fptr);
+  fclose(out_fptr);
+
+  printf("%d\n",total_count);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int remove_spaces(char *line,int line_len)
+{
+  int m;
+  int n;
+
+  for (m = 0,n = 0; n < line_len; n++) {
+    if (line[n] != ' ') {
+      if (m != n)
+        line[m++] = line[n];
+    }
+  }
+
+  line[m] = 0;
+
+  return line_len - m;
+}
